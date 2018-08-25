9c2dbacad87c1f9f4b61e3940a17d20dcf808d60
unknown <olena1q2@gmail.com>
2018-08-26 00:05:58 +0300

    register form validation (in process)

--- lightest-web/src/app/registration/registration.component.ts
+++ lightest-web/src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registrationUserForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    // check if already logged in if yes redirect to PP (personal page)
+    this.initFormValidators();
+  }
+
+  initFormValidators() {
+    this.registrationUserForm = this.formBuilder.group({
+      firstName: ['',[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(25)
+      ]],
+      secondName: ['',[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(25)
+      ]],
+      email: ['',[
+        Validators.required
+      ]],
+      login: ['',[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(25)
+      ]],
+      password: ['',[
+        Validators.required,
+        // find regualar expr for this field !!!
+      ]],
+      passwordRepeat: ['',[
+        Validators.required
+      ]]
+    })
+  }
+
+  register() {
+    console.log("Iam register");
   }
 
 }
