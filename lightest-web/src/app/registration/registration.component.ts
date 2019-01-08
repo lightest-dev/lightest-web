176b3b37e4b4890480db7aaedb2d07c90756b2a4
lllen <olena1q2@gmail.com>
2019-01-08 17:53:26 +0200

    [#23] confirm password field validation fixed on register page

--- lightest-web/src/app/registration/registration.component.ts
+++ lightest-web/src/app/registration/registration.component.ts
@@ -51,7 +51,7 @@ export class RegistrationComponent implements OnInit {
     },
     'passwordRepeat': {
       'required': `Обов'язкове поле`,
-      'notSame': `Паролі різні`
+      'compare': `Паролі не збігаються`
     }
   };
 
