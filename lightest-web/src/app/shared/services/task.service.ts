baf6b22038b3e6dea13e6b39491ffd00ca8489ef
lllen <olena1q2@gmail.com>
2019-01-13 14:00:06 +0200

    [#30] added requests for Task service

--- lightest-web/src/app/shared/services/task.service.ts
+++ lightest-web/src/app/shared/services/task.service.ts
@@ -17,5 +17,31 @@ export class TaskService {
     return this.http.post(`${API_URL}/tasks`, task);
   }
 
+  getTask(id) {
+    return this.http.get(`${API_URL}/tasks/${id}`);
+  }
 
+  changeTask(id, task) {
+    return this.http.put(`${API_URL}/tasks/${id}`, task);
+  }
+
+  deleteTask(id) {
+    return this.http.delete(`${API_URL}/tasks/${id}`);
+  }
+
+  getAssignedUsersToTask(taskId) {
+    return this.http.get(`${API_URL}/tasks/${taskId}/users`);
+  }
+
+  assignTaskToUsers(taskId, users) {
+    return this.http.post(`${API_URL}/tasks/${taskId}/users`, users);
+  }
+
+  addLanguagesForTask(taskId, languages) {
+    return this.http.post(`${API_URL}/tasks/${taskId}/languages`, languages);
+  }
+
+  addTestsForTask(taskId, tests) {
+    return this.http.post(`${API_URL}/tasks/${taskId}/tests`, tests);
+  }
 }
