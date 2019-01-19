a1cf6c41e62dfdaae43c87d40cb10ca6cbc14ece
lllen <olena1q2@gmail.com>
2019-01-20 00:26:57 +0200

    [#35]  modified Checker service and model

---
+++ lightest-web/src/app/shared/models/CheckerShort.ts
@@ -0,0 +1,6 @@
+export class CheckerShort {
+  compiled?: boolean;
+  id: string;
+  name: string;
+  code?: string;
+}
--- lightest-web/src/app/shared/services/checker.service.ts
+++ lightest-web/src/app/shared/services/checker.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCheckers(){
+
+  }
+
+  addChecker() {
+
+  }
+
+  getChecker(id){
+
+  }
+
+  changeChecker(id) {
+
+  }
+
+  deleteChecker(id){
+
+  }
+
+
 }
