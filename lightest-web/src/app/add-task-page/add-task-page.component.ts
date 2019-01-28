c262d82c77256f0213cecefa290a0709986ce7ad
Olena <olena@qlicks.nl>
2019-01-28 11:43:17 +0200

    [add-task-page] #40 add task form validation

--- lightest-web/src/app/add-task-page/add-task-page.component.ts
+++ lightest-web/src/app/add-task-page/add-task-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {TaskService} from '../shared/services/task.service';
+import {TaskShort} from '../shared/models/TaskShort';
 
 @Component({
   selector: 'app-add-task-page',
@@ -9,11 +11,40 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class AddTaskPageComponent implements OnInit {
 
-  taskForm: FormGroup;
+    taskForm: FormGroup;
+    formErrors = {
+        'taskName': '',
+        'taskPoints': '',
+        'taskDescription': '',
+        'examples': '',
+        'category': '',
+        'checker': ''
+    };
+    validationMessages = {
+        'taskName': {
+            'required': `Обов'язкове поле`
+        },
+        'taskPoints': {
+            'required': `Обов'язкове поле`
+        },
+        'taskDescription': {
+            'required': `Обов'язкове поле`
+        },
+        'examples': {
+            'required': `Обов'язкове поле`
+        },
+        'category': {
+            'required': `Обов'язкове поле`
+        },
+        'checker': {
+            'required': `Обов'язкове поле`
+        }
+    };
 
   constructor(
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private taskService: TaskService
   ) { }
 
   ngOnInit() {
@@ -34,12 +65,58 @@ export class AddTaskPageComponent implements OnInit {
       examples: ['', [
         Validators.required
       ]],
-
+      publicTask: [],
+      category:  ['', [
+          Validators.required
+      ]],
+      checker: ['', [
+          Validators.required
+      ]],
     });
+
+      this.taskForm.valueChanges
+          .subscribe(data => this.onValueChanged(data));
+
+      this.onValueChanged();
   }
 
   submit() {
-
+    console.log(this.taskForm.value);
+    // Todo: find categoryId and checkerId by its names
+    this.taskService.addNewTask(this.loadTaskObject(this.taskForm.value));
   }
 
+  loadTaskObject(currentTask): TaskShort {
+   const task: TaskShort = {
+       name: currentTask.taskName,
+       points: currentTask.taskPoints,
+       public: currentTask.publicTask,
+       checkerId: currentTask.checker,
+       categoryId: currentTask.category,
+       description: currentTask.taskDescription,
+       examples: currentTask.examples
+   };
+
+    return task;
+  }
+
+    onValueChanged(data?: any) {
+        if (!this.taskForm) { return; }
+        const form = this.taskForm;
+        for (const field in this.formErrors) {
+            if (this.formErrors.hasOwnProperty(field)) {
+                // clear previous error message (if any)
+                this.formErrors[field] = '';
+                const control = form.get(field);
+                if (control && control.dirty && !control.valid) {
+                    const messages = this.validationMessages[field];
+                    for (const key in control.errors) {
+                        if (control.errors.hasOwnProperty(key)) {
+                            this.formErrors[field] += messages[key] + ' ';
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
